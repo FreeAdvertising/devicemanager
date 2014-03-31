edbdeb6dff332438779c990200c49680411a4143
Ryan Priebe <rpriebe@me.com>
2014-03-31 16:17:57 -0600

    devices list now contains correct data when determining if a device is reserved, checked out or available

--- application/models/devices_model.php
+++ application/models/devices_model.php
@@ -7,8 +7,19 @@
 		}
 
 		public function getRecords(){
-			$query = $this->db->query("SELECT device_id, uuid, status, os FROM device_manager_devices ORDER BY device_id");
+			$query = $this->db->query("SELECT 
+				d.device_id, 
+				d.uuid, 
+				d.status, 
+				d.os, 
+				IF(ar.ass_id, 'co', 'ci') as checkout_status, 
+				IF(rr.res_id, 're', 'av') as reserved_status
+				FROM device_manager_devices d
+				LEFT JOIN device_manager_assignments_rel ar ON ar.device_id = d.device_id 
+				LEFT JOIN device_manager_reservations_rel rr ON rr.device_id = d.device_id 
+				ORDER BY device_id");
 
+			var_dump($query->result_object());
 			if($query->num_rows() > 0){
 				return $query->result_object();
 			}
