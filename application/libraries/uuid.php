b57855e076e4b8f8c365b89b1ada4db845c027c4
Ryan Priebe <rpriebe@me.com>
2014-03-31 14:17:37 -0600

    added the UUID class

---
+++ application/libraries/uuid.php
@@ -0,0 +1,58 @@
+<?php
+
+	defined("BASEPATH") or die;
+
+	class UUID {
+		protected $uuid = "INVALIDID";
+		protected $ci;
+
+		public function __construct($uuid = null){
+			$this->ci = get_instance();
+			$this->set($uuid);
+
+			return $this;
+		}
+
+		public function set($val){
+			if(false === is_null($val) && strlen($val) === 8){
+				$this->uuid = strtoupper($val);
+			}
+
+			return false;
+		}
+
+		public function get(){
+			return $this->uuid;
+		}
+
+		public function isInstance($uuid){
+			return ($uuid instanceof self);
+		}
+
+		/**
+		 * Convert a generic string to a UUID object
+		 * @param  [type] $uuid
+		 * @return [type]
+		 */
+		public function convert(){
+			try {
+				if(false === $this->isInstance($this->uuid)){
+					$query = $this->ci->db->query("SELECT device_id FROM device_manager_devices WHERE uuid = ? LIMIT 1", array($this->uuid));
+
+					if(sizeof($query->result_object()) > 0){
+						return $this;
+					}
+				}else {
+					throw new Exception("Invalid UUID");
+				}
+			}catch(Exception $e){
+				show_error($e->getMessage());
+			}
+		}
+
+		public function __toString(){
+			return $this->uuid;
+		}
+	}
+
+?>
