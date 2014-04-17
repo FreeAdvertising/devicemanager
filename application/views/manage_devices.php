30d1887dc8ead00e9ec9c2dca772e6783c29d9a0
Ryan Priebe <ryan@wearefree.ca>
2014-04-17 14:56:23 -0600

    whoops, this got overwritten.. manage devices view should link directly to the device edit page rather than the device view page

--- application/views/manage_devices.php
+++ application/views/manage_devices.php
@@ -13,9 +13,9 @@
 					<?php $status = $this->product->get_status($records[$i]); ?>
 					<tr data-location="<?php echo $records[$i]->current_owner; ?>" data-status="<?php echo $status; ?>">
 						<?php if($records[$i]->name): ?>
-							<td><?php echo anchor(sprintf("/device/%s", strtoupper($records[$i]->uuid)), sprintf("%s (%s)", $records[$i]->name, $records[$i]->uuid)); ?></td>
+							<td><?php echo anchor(sprintf("/device/%s/edit", strtoupper($records[$i]->uuid)), sprintf("%s (%s)", $records[$i]->name, $records[$i]->uuid)); ?></td>
 						<?php else : ?>
-							<td><?php echo anchor(sprintf("/device/%s", strtoupper($records[$i]->uuid)), sprintf("%s", $records[$i]->uuid)); ?></td>
+							<td><?php echo anchor(sprintf("/device/%s/edit", strtoupper($records[$i]->uuid)), sprintf("%s", $records[$i]->uuid)); ?></td>
 						<?php endif; ?>
 
 						<td><?php echo anchor(sprintf("/device/%s/apps", $records[$i]->uuid), "View"); ?></td>
