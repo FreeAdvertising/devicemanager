84b712edfc6cfabe793d376d5fcd825526ccdb84
Ryan Priebe <ryan@wearefree.ca>
2014-05-02 14:38:18 -0600

    Removed index method from Modify_device controller as there will be no landing page

--- application/controllers/modify_device.php
+++ application/controllers/modify_device.php
@@ -8,34 +8,6 @@
 			$this->load->model("modify_device_model");
 		}
 
-		public function index(){
-			die("TEST");
-			if(false === $this->hydra->isAdmin()){
-				show_error("You do not have permission to view this page.");
-			}
-
-			$data = new Generic;
-			$data->set("template_path", base_url() ."application/views/global");
-			$data->set("nav_path", base_url() ."index.php/");
-			$data->set("page", $this->uri->segment(1));
-			$data->set("subpage", $this->uri->segment(2));
-			$data->set("isIPExternal", $this->hydra->isIPExternal());
-
-			//set specific page data
-			$data->set("users", $this->add_device_model->getUsers());
-
-			//load the relevant views
-			$this->load->view('header', $data);
-			
-			if($this->hydra->isAuthenticated()){
-				$this->load->view('form_add_device');
-			}else {
-				$this->load->view("login", $data);
-			}
-
-			$this->load->view('footer', $data);
-		}
-
 		public function edit(){
 			if(false === $this->hydra->isAdmin()){
 				show_error("You do not have permission to view this page.");
